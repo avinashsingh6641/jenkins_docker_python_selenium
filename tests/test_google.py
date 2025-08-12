0bc9de9512a60f3e875125a9768f56b084b0014d
avinashsingh6641 <83644393+avinashsingh6641@users.noreply.github.com>
2025-08-12 13:03:26 +0000

    12-08-2025_18-33

--- tests/test_google.py
+++ tests/test_google.py
@@ -1,26 +1,28 @@
-# tests/test_google.py
-
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
+# Chrome options
+options = Options()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--headless=new")  # Remove this if you want to see browser
 
-def test_google():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
+# Start Chrome directly
+driver = webdriver.Chrome(options=options)
 
-    driver = webdriver.Remote(
-        command_executor='http://localhost:4444/wd/hub',
-        options=options
-    )
-
-    # Open Google
+try:
     driver.get("https://www.google.com")
+    print("Page title:", driver.title)
 
-    # Assert page title contains "Google"
-    assert "Google" in driver.title
-    print(f"hjhjdfhjdf {driver.title}")
+    search_box = driver.find_element(By.NAME, "q")
+    search_box.send_keys("Hello from Selenium!")
+    search_box.send_keys(Keys.RETURN)
 
-    # Quit browser
+    time.sleep(2)  # Let the results load
+    print("New page title:", driver.title)
+
+finally:
     driver.quit()
